9ecf82348cccd5efa567aaf1ba4ad4067cc9c4d2
nsallin <nicolas.sallin@unibe.ch>
2025-11-07 15:36:25 +0100

    removed old files

--- plots_distance_correction.py
+++
@@ -1,8 +0,0 @@
-import ROOT as root
-import os.path
-#Adjust filename to create plots of different scans
-filename = "FCLM_01_02_03_2_2_stabConv.root"
-
-file = root.TFile.Open(filename,"READ")
-
-
--- start_daq_watchdog.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-/home/lhep/afi-new/build/adc64-system/afi-adc64-system --watchdog_file /home/lhep/.adc_watchdog_file --data_dir /data/3dscan/data_files
